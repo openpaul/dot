2bb94df7c559b77236ab089494c2803ec510d6ed
Paul Saary <saary@ebi.ac.uk>
2019-11-11 20:22:50 +0000

    update all configs together, why not

--- software/dot/install.sh
+++
@@ -1,9 +0,0 @@
-sudo apt install gmrun vim 
-
-# configuring network manager to use DNS
-sudo rm /etc/NetworkManager/NetworkManager.conf
-sudo ln -s $PWD/etc/NetworkManager/NetworkManager.conf /etc/NetworkManager/
-sudo /etc/init.d/network-manager restart
-
-
-
